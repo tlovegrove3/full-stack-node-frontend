0c4b33b2f4c5802a009ef5c0362788c072b44758
tlovegrove3 <tlovegrove3@ivytech.edu>
2025-07-08 00:39:17 -0400

    Updated fetch to pull from the render /api/songs site

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //lets make an event listener. It will trigger when the DOM is loaded
 
 addEventListener("DOMContentLoaded", async function (){
-    const response = await this.fetch("http://localhost:3000/api/songs")
+    // const response = await fetch("http://localhost:3000/api/songs")
+    const response = await fetch("https://full-stack-node-backend.onrender.com/api/songs")
     const songs = await response.json()
 
     let html = ""
